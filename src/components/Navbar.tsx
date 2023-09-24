2aaf1c3b58e8e3dd85f4bcb956ba0cc1f45f7c2d
epicdevler <dev.epicdevler@gmail.com>
2023-09-24 02:59:02 +0100

    improved navbar layout  and added navbar toggle functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
+'use client'
 import { Box, Container, Text, Flex, HStack, IconButton, VStack, SystemStyleObject, Button } from "@chakra-ui/react"
-import { MagnifyingGlassIcon, Bars3Icon, ShoppingCartIcon } from "@heroicons/react/24/outline"
+import { MagnifyingGlassIcon, Bars3Icon, XCircleIcon, ShoppingCartIcon } from "@heroicons/react/24/outline"
 import Link from "next/link"
-// import {} from '@heroicons/react/2'
+import { useState } from "react"
 
 
 const siteMap = [
@@ -34,17 +35,47 @@ const _iconBtnActiveEff: SystemStyleObject = {
 }
 
 
-export function NavbarFull() {
-    return <nav className={'absolute top-0 right-0 bg-[rgba(0, 0, 0, 85)] h-screen w-full md:hidden z-40'}>
+function BookTaleBtn({ hideBelow }: { hideBelow?: "md" }) {
+    return <Button hideBelow={hideBelow}>
+        Book a Table
+    </Button>
+}
 
-        <VStack textColor='white' spacing={5} as={'ul'} hideBelow={'md'} fontSize={14}>
-            {
-                siteMap.map(
-                    (item, index) => {
-                        return <Text key={index} as={'li'} py={1}><Link href={item.href}>{item.label}</Link></Text>
+export function NavbarFull(
+    { show = false, onClose }: { show: boolean, onClose: () => void }
+) {
+
+    
+
+    return <nav className={`fixed transition-all duration-150 w-full ${show ? "-" : ""}translate-x-full bg-white top-0 right-0 bg-[rgba(0, 0, 0, 85)] h-screen md:hidden p-5 z-50`}>
+        <VStack spacing={10} >
+            <Box w={'full'} textAlign={'end'}>
+                <IconButton
+                    onClick={onClose}
+                    colorScheme='black'
+                    variant={'outlined'}
+                    isRound={true}
+                    size={'sm'}
+                    aria-label='Close Navigation Menu'
+                    icon={
+                        <XCircleIcon className="h-5 w-5" />
                     }
-                )
-            }
+                />
+            </Box>
+
+            <VStack spacing={5} as={'ul'} fontSize={14}>
+
+                {
+                    siteMap.map(
+                        (item, index) => {
+                            return <Text key={index} as={'li'} py={1}><Link href={item.href}>{item.label}</Link></Text>
+                        }
+                    )
+                }
+
+            </VStack>
+
+            <BookTaleBtn />
         </VStack>
 
     </nav>
@@ -52,64 +83,79 @@ export function NavbarFull() {
 
 
 const Navbar = () => {
-    return <nav>
-        <Container maxW={'container.lg'} py={4} textColor='white'>
-            <Flex alignItems={'center'} justifyContent='space-between'>
-                <Text>
-                    Logo
-                </Text>
+    const [isNavbarShown, setIsNavbarShown] = useState(false)
 
-                <HStack spacing={5} as={'ul'} hideBelow={'md'} fontSize={14}>
-                    {
-                        siteMap.map(
-                            (item, index) => {
-                                return <Text key={index} as={'li'} py={1}><Link href={item.href}>{item.label}</Link></Text>
+    const handleNavbarToggle = () => {
+        setIsNavbarShown(!isNavbarShown)
+    }
+
+    return <>
+        <nav>
+            <Container maxW={'container.lg'} py={4} textColor='white'>
+                <Flex alignItems={'center'} justifyContent='space-between'>
+                    <Text>
+                        Logo
+                    </Text>
+
+                    <HStack spacing={5} as={'ul'} hideBelow={'md'} fontSize={14}>
+                        {
+                            siteMap.map(
+                                (item, index) => {
+                                    return <Text key={index} as={'li'} py={1}><Link href={item.href}>{item.label}</Link></Text>
+                                }
+                            )
+                        }
+                    </HStack>
+
+                    <HStack spacing={3}>
+                        <IconButton
+                            _active={_iconBtnActiveEff}
+                            transition={"border .2s ease-in-out"}
+                            _hover={_iconBtnHoverEff}
+                            aria-label="search"
+                            borderRadius='full'
+                            bg={'transparent'}
+                            icon={
+                                <MagnifyingGlassIcon
+                                    className="h-4 w-4 text-white" />
                             }
-                        )
-                    }
-                </HStack>
+                        />
+                        <IconButton
+                            _active={_iconBtnActiveEff}
+                            transition={"border .2s ease-in-out"}
+                            _hover={_iconBtnHoverEff}
+                            aria-label="My Cart"
+                            borderRadius='full'
+                            bg={'transparent'}
+                            icon={
 
-                <HStack spacing={5}>
-                    <IconButton
-                        _active={_iconBtnActiveEff}
-                        transition={"border .2s ease-in-out"}
-                        _hover={_iconBtnHoverEff}
-                        aria-label="search"
-                        borderRadius='full'
-                        bg={'transparent'}>
-                        <MagnifyingGlassIcon
-                            className="h-4 w-4 text-white" />
-                    </IconButton>
-                    <IconButton
-                        _active={_iconBtnActiveEff}
-                        transition={"border .2s ease-in-out"}
-                        _hover={_iconBtnHoverEff}
-                        aria-label="My Cart"
-                        borderRadius='full'
-                        bg={'transparent'}>
-                        <ShoppingCartIcon
-                            className="h-4 w-4 text-white" />
-                    </IconButton>
+                                <ShoppingCartIcon
+                                    className="h-4 w-4 text-white" />
+                            }
+                        />
 
-                    <IconButton
-                        _active={_iconBtnActiveEff}
-                        transition={"border .2s ease-in-out"}
-                        _hover={_iconBtnHoverEff}
-                        aria-label="Open Navigation Menu"
-                        hideFrom={'md'}
-                        borderRadius='full'
-                        bg={'transparent'}>
-                        <Bars3Icon
-                            className="h-4 w-4 text-white" />
-                    </IconButton>
+                        <IconButton
+                            onClick={handleNavbarToggle}
+                            _active={_iconBtnActiveEff}
+                            transition={"border .2s ease-in-out"}
+                            _hover={_iconBtnHoverEff}
+                            aria-label="Open Navigation Menu"
+                            hideFrom={'md'}
+                            borderRadius='full'
+                            bg={'transparent'}
+                            icon={
+                                <Bars3Icon
+                                    className="h-4 w-4 text-white" />
+                            }
+                        />
+                        <BookTaleBtn hideBelow="md" />
+                    </HStack>
+                </Flex>
+            </Container>
+        </nav>
 
-                    <Button hideBelow={'md'}>
-                        Book a Table
-                    </Button>
-                </HStack>
-            </Flex>
-        </Container>
-    </nav>
+        <NavbarFull show={isNavbarShown} onClose={handleNavbarToggle} />
+    </>
 }
 
 export default Navbar
