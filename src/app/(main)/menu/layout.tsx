75bfb458567e42f71ae505b797d0690e43a665e1
epicdevler <dev.epicdevler@gmail.com>
2023-09-27 21:43:39 +0100

    Attempted to fix error warn Entire page / deopted into client-side rendering

--- src/app/(main)/menu/layout.tsx
+++ src/app/(main)/menu/layout.tsx
@@ -1,12 +1,10 @@
+import MenuHeroSection from '@/app/(main)/menu/_sections/_hero'
 
-import MenuHeroSection from './_sections/_hero';
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-
-    
 
     return (
         <>
